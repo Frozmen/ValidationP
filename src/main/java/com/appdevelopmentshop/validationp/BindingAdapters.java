d5a0c956ebe5cfcca14acbf296a0c1280a22c6d4
frozmen <sisetskyi.a@gmail.com>
2018-07-16 17:22:24 +0300

    reformat code style

--- src/main/java/com/appdevelopmentshop/validationp/BindingAdapters.java
+++ src/main/java/com/appdevelopmentshop/validationp/BindingAdapters.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import com.appdevelopmentshop.validationp.conditions.Condition;
 import com.appdevelopmentshop.validationp.rules.Rule;
 
-
 /**
  * Created by Artem Sisetskyi on 6/14/18.
  * AppDevelopmentShop
@@ -16,41 +15,42 @@ import com.appdevelopmentshop.validationp.rules.Rule;
  */
 public class BindingAdapters {
 
-    public static final String TAG = BindingAdapters.class.getSimpleName();
+  public static final String TAG = BindingAdapters.class.getSimpleName();
 
-    @BindingAdapter(value = {"validator", "rules"}, requireAll = false)
-    public static void _bindValidatorRules(final View view,
-                                     Validator validator,  Rule... rules) {
-        if (validator == null) {
-            Log.i(TAG, "_bindValidatorRules: validator is null");
-            return;
-        }
-
-        Condition cond = ConditionFabric.getCondition(view, rules);
-        if (cond == null) {
-            Log.i(TAG, "_bindValidatorRules: there is no default condition for view " + view.getClass());
-            //todo remove in prod
-            throw new IllegalStateException("_bindValidatorRules: there is no condition for view " + view.getClass());
-        }
-        validator.addCondition(cond);
+  @BindingAdapter(value = {"validator", "rules"}, requireAll = false)
+  public static void _bindValidatorRules(final View view,
+      Validator validator, Rule... rules) {
+    if (validator == null) {
+      Log.i(TAG, "_bindValidatorRules: validator is null");
+      return;
     }
 
-    @BindingAdapter(value = {"validator", "conditions"}, requireAll = false)
-    public static void _bindValidatorCondition(final View view,
-                                     Validator validator, Condition conditions) {
-        if (validator == null || conditions == null) {
-            Log.i(TAG, "_bindValidatorCondition: validator or condition is null");
-            return;
-        }
-        validator.addCondition(conditions);
+    Condition cond = ConditionFabric.getCondition(view, rules);
+    if (cond == null) {
+      Log.i(TAG, "_bindValidatorRules: there is no default condition for view " + view.getClass());
+      //todo remove in prod
+      throw new IllegalStateException(
+          "_bindValidatorRules: there is no condition for view " + view.getClass());
     }
+    validator.addCondition(cond);
+  }
 
-    @BindingAdapter("initValidator")
-    public static void _initValidator(final ViewGroup view, ValidatorReadyListener validatoristener) {
-        validatoristener.onValidatorReady(new Validator(true), view.getRootView());
+  @BindingAdapter(value = {"validator", "conditions"}, requireAll = false)
+  public static void _bindValidatorCondition(final View view,
+      Validator validator, Condition conditions) {
+    if (validator == null || conditions == null) {
+      Log.i(TAG, "_bindValidatorCondition: validator or condition is null");
+      return;
     }
+    validator.addCondition(conditions);
+  }
 
-    public interface ValidatorReadyListener{
-        void onValidatorReady(Validator validator, View root);
-    }
+  @BindingAdapter("initValidator")
+  public static void _initValidator(final ViewGroup view, ValidatorReadyListener validatoristener) {
+    validatoristener.onValidatorReady(new Validator(true), view.getRootView());
+  }
+
+  public interface ValidatorReadyListener {
+    void onValidatorReady(Validator validator, View root);
+  }
 }
